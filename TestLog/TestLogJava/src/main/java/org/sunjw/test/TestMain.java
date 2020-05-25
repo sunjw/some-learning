f37e0017de74ea08e037b1e0d8f8dc8e07006f78
Sun Junwen <sunjw8888@gmail.com>
2020-05-25 13:22:19 +0800

    TestLog: TestLogJava tweak log format.

--- TestLog/TestLogJava/src/main/java/org/sunjw/test/TestMain.java
+++ TestLog/TestLogJava/src/main/java/org/sunjw/test/TestMain.java
@@ -1,9 +1,15 @@
 package org.sunjw.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TestMain {
 
-    public static void main(String args[]) {
+    private static Logger logger = LoggerFactory.getLogger(TestMain.class);
 
+    public static void main(String args[]) {
+        logger.info("main start...");
+        logger.info("main end.");
     }
 
 }
