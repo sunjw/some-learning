b39b10496193451b13f620904df3b5cd5637148f
Sun Junwen <sunjw8888@gmail.com>
2021-08-05 12:54:26 +0800

    sqlite-edit-distance: Update comm_util.

--- sqlite-edit-distance/comm_util.py
+++ sqlite-edit-distance/comm_util.py
@@ -1,14 +1,25 @@
 import datetime
+import hashlib
 import io
 import json
 import math
 import os
+import platform
 import re
+import subprocess
 import time
+
+
+def log_print(message):
+    print(message)
 
 
 def get_cur_timestamp():
     return math.floor(time.mktime(datetime.datetime.today().timetuple()))
+
+
+def get_cur_timestamp_millis():
+    return int(round(time.time() * 1000))
 
 
 def read_file_text(file_path):
@@ -47,3 +58,30 @@ def is_ipv4(input_str):
         return True
     else:
         return False
+
+
+def list_file(dir_path):
+    files = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+    return files
+
+
+def is_windows():
+    return (platform.system() == 'Windows')
+
+
+def is_macos():
+    return (platform.system() == 'Darwin')
+
+
+def is_linux():
+    return (platform.system() == 'Linux')
+
+
+def call_command(args_list, is_shell=False):
+    subprocess.call(args_list, shell=is_shell)
+
+
+def md5_str(input_str, encode='utf-8'):
+    md5hash = hashlib.md5()
+    md5hash.update(input_str.encode(encode))
+    return md5hash.hexdigest()
