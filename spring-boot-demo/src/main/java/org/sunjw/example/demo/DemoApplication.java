76d807274a6fa8e607f677baba5be6b0a95c586c
Sun Junwen <sunjw8888@gmail.com>
2021-08-05 17:03:19 +0800

    spring-boot-demo: Add jdbc example.

--- spring-boot-demo/src/main/java/org/sunjw/example/demo/DemoApplication.java
+++ spring-boot-demo/src/main/java/org/sunjw/example/demo/DemoApplication.java
@@ -2,24 +2,34 @@ package org.sunjw.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.sunjw.example.demo.consumingrest.Quote;
+import org.sunjw.example.demo.relationaldataaccess.Customer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableScheduling
 @RestController
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -42,5 +52,32 @@ public class DemoApplication {
                     "https://quoters.apps.pcfone.io/api/random", Quote.class);
             log.info(quote.toString());
         };
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // Use a Java 8 stream to print out each tuple of the list
+        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        log.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name")),
+                "Josh"
+        ).forEach(customer -> log.info(customer.toString()));
     }
 }
