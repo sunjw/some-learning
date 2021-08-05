5c6769c72904e50d110424910929dc2a441bef30
Sun Junwen <sunjw8888@gmail.com>
2021-08-05 16:31:55 +0800

    spring-boot-demo: Add rest consumer.

--- spring-boot-demo/src/main/java/org/sunjw/example/demo/DemoApplication.java
+++ spring-boot-demo/src/main/java/org/sunjw/example/demo/DemoApplication.java
@@ -1,16 +1,25 @@
 package org.sunjw.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.sunjw.example.demo.consumingrest.Quote;
 
 @SpringBootApplication
 @EnableScheduling
 @RestController
 public class DemoApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -21,4 +30,17 @@ public class DemoApplication {
         return String.format("Hello %s!", name);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://quoters.apps.pcfone.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
 }
