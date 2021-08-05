d482ad22147147809112476b4b651dc512ffa8ab
Sun Junwen <sunjw8888@gmail.com>
2021-08-05 16:14:31 +0800

    spring-boot-demo: Add ScheduledTasks.

--- spring-boot-demo/src/main/java/org/sunjw/example/demo/DemoApplication.java
+++ spring-boot-demo/src/main/java/org/sunjw/example/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package org.sunjw.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableScheduling
 @RestController
 public class DemoApplication {
 
