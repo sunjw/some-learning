4b16fde0aab96d63f2cd9c3312ff46e332fea92e
Sun Junwen <sunjw8888@gmail.com>
2021-08-09 18:45:39 +0800

    spring-boot-demo-eureka: Add eureka server example.

---
+++ spring-boot-demo-eureka/spring-boot-demo-eureka-server/src/main/java/org/sunjw/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package org.sunjw.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
