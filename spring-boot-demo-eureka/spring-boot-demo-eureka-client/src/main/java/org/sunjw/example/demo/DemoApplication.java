d76500f5cf8cae95b62edc65fd0f4ca9926fba7a
Sun Junwen <sunjw8888@gmail.com>
2021-08-09 19:52:03 +0800

    spring-boot-demo-eureka: Add eureka client example.

---
+++ spring-boot-demo-eureka/spring-boot-demo-eureka-client/src/main/java/org/sunjw/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package org.sunjw.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    class ServiceInstanceRestController {
+
+        @Autowired
+        private DiscoveryClient discoveryClient;
+
+        @RequestMapping("/service-instances/{applicationName}")
+        public List<ServiceInstance> serviceInstancesByApplicationName(
+                @PathVariable String applicationName) {
+            return this.discoveryClient.getInstances(applicationName);
+        }
+    }
+
+}
