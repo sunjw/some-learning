b61968e73da6dcc12ce4f3df91347b74c03c6730
Sun Junwen <sunjw8888@gmail.com>
2022-01-12 18:42:48 +0800

    wallpicker: Update package script.

--- wallpicker/pack-dist-electron.py
+++ wallpicker/pack-dist-electron.py
@@ -101,7 +101,7 @@ APP_FILES = ['index.html', 'main.js',
             'README.md', 'LICENSE']
 
 EXEC_FIX_PATHS = ['./node_modules/.bin/electron-rebuild',
-                    './node_modules/wallpaper/source/macos-wallpaper']
+                './node_modules/wallpaper/source/macos-wallpaper']
 
 REBUILD_CMD = ''
 REBUILD_CLEAN_PATHS_WIN = []
