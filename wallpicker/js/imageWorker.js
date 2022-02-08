86be76622050526af3f43efd0f872b20a2a1f36b
Sun Junwen <sunjw8888@gmail.com>
2022-02-08 18:07:55 +0800

    wallpicker: Implement generateImageThumbnail.

--- wallpicker/js/imageWorker.js
+++ wallpicker/js/imageWorker.js
@@ -2,10 +2,49 @@ const fs = require('fs');
 const path = require('path');
 
 const sharp = require('sharp');
-
 const utils = require('./utils');
 
+function generateImageThumbnail(options) {
+    utils.log('generateImageThumbnail, options=\n%s', JSON.stringify(options, null, 2));
+
+    let imageSrcPath = options.imageSrcPath;
+    let imageThumbPath = options.imageThumbPath;
+    let imageThumbWidth = options.imageThumbWidth;
+    let imageThumbHeight = options.imageThumbHeight;
+    let imageThumbFormat = options.imageThumbFormat;
+
+    let sharpImage = sharp(imageSrcPath).resize({
+        width: imageThumbWidth,
+        height: imageThumbHeight,
+        fit: 'fill'
+    });
+    switch (imageThumbFormat) {
+    case 'jpg':
+        sharpImage.jpeg();
+        break;
+    case 'png':
+        sharpImage.png();
+        break;
+    }
+    sharpImage.toFile(imageThumbPath, function (err, info) {
+        if (err) {
+            utils.log('generateImageThumbnail, error=[%s]', err);
+            postMessage({
+                'err': err
+            });
+            return;
+        }
+
+        utils.log('generateImageThumbnail, thumbnail generated, imageThumbPath=[%s]', imageThumbPath);
+        postMessage({
+            'imageSrcPath': imageSrcPath,
+            'imageThumbPath': imageThumbPath
+        });
+    });
+}
+
 onmessage = function (e) {
-    utils.log('imageWorker.onmessage, [%s]', e.data);
-    postMessage('Worker!');
+    // utils.log('imageWorker.onmessage, [%s]', e.data);
+    // postMessage('Worker!');
+    generateImageThumbnail(e.data);
 };
