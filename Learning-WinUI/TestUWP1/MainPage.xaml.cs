c6e769083f7da5cc775b0085ace17fe6ff403962
Sun Junwen <sunjw8888@gmail.com>
2022-05-24 15:20:29 +0800

    Learning-WinUI: Try add content to RichTextBlock.

--- Learning-WinUI/TestUWP1/MainPage.xaml.cs
+++ Learning-WinUI/TestUWP1/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -140,11 +141,18 @@ namespace TestUWP1
 
         private void GridRoot_Loaded(object sender, RoutedEventArgs e)
         {
+            // Theme changed callback
             m_uiSettings = new UISettings();
             Frame rootFrame = (Frame)Window.Current.Content;
             m_uiSettings.ColorValuesChanged += ColorValuesChanged;
             // RequestedThemeProperty seems not work at all...
             m_tokenThemeChanged = rootFrame.RegisterPropertyChangedCallback(RequestedThemeProperty, RequestedThemeChanged);
+
+            // Init ui
+            RichTextMain.TextWrapping = TextWrapping.NoWrap;
+
+            // Test content
+            InitTestContent();
         }
 
         private void ButtonTest_Click(object sender, RoutedEventArgs e)
@@ -158,6 +166,37 @@ namespace TestUWP1
         private void ButtonAbout_Click(object sender, RoutedEventArgs e)
         {
             ShowPopupAbout();
+        }
+
+        private void InitTestContent()
+        {
+            Paragraph paragraph = new Paragraph();
+
+            Run run1 = new Run();
+            string strRun1 = "文件名: C:\\Users\\Sun Junwen\\OneDrive\\Apps\\fHash\\fHash64-2.3.0-win64.zip\r\n";
+            strRun1 += "文件大小: 383692 字节(374.70 KB)\r\n";
+            strRun1 += "修改日期: 2022-03-28 15:17\r\n";
+            strRun1 += "MD5: c0232b5bc37e70eebad95fbfc6499d92\r\n";
+            strRun1 += "SHA1: 14017068424f5ca356b9440cb025eb0f848e08d9\r\n";
+            strRun1 += "SHA256: ce25ecf0650ebfa1f8f2d8ebc557ad3db0cd75e80bda647c59314668e0281a35\r\n";
+            strRun1 += "SHA512: aeda1930fc0ae1feda19b68170b78074f4a408ec50080256110dd0d9eda005abaa7167ae6d62ca302f6995f60f3d038af6c21667ea922e1206bb3670bc1c5e71\r\n";
+            strRun1 += "\r\n";
+            run1.Text = strRun1;
+            paragraph.Inlines.Add(run1);
+
+            Run run2 = new Run();
+            string strRun2 = "Name: /Users/sunjw/Library/CloudStorage/OneDrive-个人/Apps/fHash/fHash-2.3.0-macOS.dmg\r\n";
+            strRun2 += "File Size: 656793 Byte(s)(656.79 KB)\r\n";
+            strRun2 += "Modified Date: 2022-03-28 14:58\r\n";
+            strRun2 += "MD5: 1C880E1191F884EB225F9CFA36CDC355\r\n";
+            strRun2 += "SHA1: 4528265C32082A3FB115C05CB29FA36E96A942E5\r\n";
+            strRun2 += "SHA256: B704FD5E9E6818F1AC8E7EBECF97F0A56E1FDB8DB9AB9E01FE0F6B574A586FEF\r\n";
+            strRun2 += "SHA512: 15F634DF65D0A41E943A9024CA69FF5C31E4B05446180FE5900B7CB0068CFF9DF2820EFD3F8AFCB48924C3FE45D0113FDC8E81F5E4E6CDD60830BE4581F8B11C\r\n";
+            strRun2 += "\r\n";
+            run2.Text = strRun2;
+            paragraph.Inlines.Add(run2);
+
+            RichTextMain.Blocks.Add(paragraph);
         }
 
     }
