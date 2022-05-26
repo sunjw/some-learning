69b862d01b67dfb9fa1e27592a6ce9dc4755dc4b
Sun Junwen <sunjw8888@gmail.com>
2022-05-26 10:23:16 +0800

    Learning-WinUI: Change UUID of TestShellExt.

--- Learning-WinUI/TestUWP1/TestShellExt/Dll.h
+++ Learning-WinUI/TestUWP1/TestShellExt/Dll.h
@@ -16,7 +16,7 @@ void DllRelease();
 
 // use UUDIGEN.EXE to generate unique CLSID values for your objects
 
-class __declspec(uuid("dd2a27fa-7c7f-4b50-9b54-836af42fb64d")) CExplorerCommandVerb;
+class __declspec(uuid("0C88A228-0DEC-4E45-B801-96EC8E4BC4B5")) CExplorerCommandVerb;
 
 HRESULT CExplorerCommandVerb_CreateInstance(REFIID riid, void **ppv);
 HRESULT CExplorerCommandVerb_RegisterUnRegister(bool fRegister);
