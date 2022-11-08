e203c99038f14bfbbeeeec1c766c437ae246bc7b
Sun Junwen <sunjw8888@gmail.com>
2022-11-08 14:00:07 +0800

    List2Yaml: Sort yaml.

--- List2Yaml/list_to_yaml.py
+++ List2Yaml/list_to_yaml.py
@@ -1,3 +1,4 @@
+import functools
 import os
 import re
 import sys
@@ -10,10 +11,46 @@ import log_util
 logger = log_util.logger
 
 
+def sort_cmp_str(str1, str2):
+    if str1 > str2:
+        return 1
+    if str1 < str2:
+        return -1
+    return 0
+
+
+def get_dict_key0(a_dict):
+    keys_list = list(a_dict.keys())
+    return keys_list[0]
+
+
+def list_sort_func(elem1, elem2):
+    if isinstance(elem1, dict) and isinstance(elem2, dict):
+        elem1_key = get_dict_key0(elem1)
+        elem2_key = get_dict_key0(elem2)
+        return sort_cmp_str(elem1_key, elem2_key)
+    if isinstance(elem1, dict):
+        return -1
+    if isinstance(elem2, dict):
+        return 1
+    return sort_cmp_str(elem1, elem2)
+
+
+def sort_yaml_list_recursive(yaml_list):
+    yaml_list.sort(key=functools.cmp_to_key(list_sort_func))
+    for list_item in yaml_list:
+        if isinstance(list_item, dict):
+            list_item_key = get_dict_key0(list_item)
+            sort_yaml_list_recursive(list_item[list_item_key])
+
+
 def sort_yaml(list_yaml_path):
     list_yaml_content = comm_util.read_file_text(list_yaml_path)
     list_yaml_dict = yaml.safe_load(list_yaml_content)
     # print(list_yaml_dict)
+
+    root_key = get_dict_key0(list_yaml_dict)
+    sort_yaml_list_recursive(list_yaml_dict[root_key])
 
     list_yaml_content_new = yaml.dump(list_yaml_dict, allow_unicode=True)
     comm_util.write_file_text(list_yaml_path, list_yaml_content_new)
