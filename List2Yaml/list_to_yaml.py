fe9a50088ddb5cd6141018976e205845509d12a5
Sun Junwen <sunjw8888@gmail.com>
2022-11-10 10:13:00 +0800

    List2Yaml: Some refactor.

--- List2Yaml/list_to_yaml.py
+++ List2Yaml/list_to_yaml.py
@@ -148,6 +148,15 @@ def list_to_yaml(list_txt_path, list_yaml_path):
     comm_util.write_file_text(list_yaml_path, list_yaml_content)
 
 
+def list_to_sort_yaml(list_txt_path, list_yaml_path):
+    list_txt_path = os.path.realpath(sys.argv[1])
+    list_yaml_path = generate_yaml_path(list_txt_path)
+    logger.info('list_to_sort_yaml, [%s] -> [%s]', list_txt_path, list_yaml_path)
+    list_to_yaml(list_txt_path, list_yaml_path)
+
+    sort_yaml(list_yaml_path)
+
+
 def main():
     logger.info('List2Yaml go!')
 
@@ -157,10 +166,7 @@ def main():
 
     list_txt_path = os.path.realpath(sys.argv[1])
     list_yaml_path = generate_yaml_path(list_txt_path)
-    logger.info('List2Yaml, [%s] -> [%s]', list_txt_path, list_yaml_path)
-    list_to_yaml(list_txt_path, list_yaml_path)
-
-    sort_yaml(list_yaml_path)
+    list_to_sort_yaml(list_txt_path, list_yaml_path)
 
 
 if __name__ == '__main__':
