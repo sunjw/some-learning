a6a597bbe1a997ef3128e3fbb5f2ccfab1939288
Sun Junwen <sunjw8888@gmail.com>
2023-07-13 21:07:25 +0800

    BingWallpaper: Add download all PowerShell script.

---
+++ BingWallpaper/download-bing-wallpaper-all.ps1
@@ -0,0 +1,17 @@
+$MktArr = @("en-us", "zh-cn", "ja-jp", "fr-fr", "it-it", "xx-xx")
+
+$FirstMkt = $true
+foreach ($MktItr in $MktArr) {
+    $ArgNoPromote = "--no-promote"
+    if ($FirstMkt) {
+        $ArgNoPromote = ""
+        $FirstMkt = $false
+    }
+    $ArgMkt=" --mkt $MktItr"
+
+    $DownloadArgs="$ArgMkt $ArgNoPromote"
+    # Write-Host $DownloadArgs
+
+    # python3 bing_wallpaper.py $DownloadArgs
+    Start-Process "python3" -ArgumentList "bing_wallpaper.py $DownloadArgs" -NoNewWindow -PassThru -Wait
+}
