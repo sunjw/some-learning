649386d7ec53087efd1e2bd55c7d247b14bc286a
Sun Junwen <sunjw8888@gmail.com>
2023-07-13 18:01:39 +0800

    BingWallpaper: Add download all bash script.

---
+++ BingWallpaper/download_bing_wallpaper_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## declare an array variable
+declare -a mkt_arr=("en-us" "zh-cn" "ja-jp" "fr-fr" "it-it" "xx-xx")
+
+first_mkt=true
+## now loop through the above array
+for mkt_itr in "${mkt_arr[@]}"
+do
+    arg_no_promote="--no-promote"
+    if [ "$first_mkt" = true ] ; then
+        arg_no_promote=""
+        first_mkt=false
+    fi
+    arg_mkt=" --mkt $mkt_itr"
+
+    download_args="$arg_mkt $arg_no_promote"
+    echo "$download_args"
+
+    python3 bing_wallpaper.py $download_args
+done
