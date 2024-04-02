73871a0923cb335f0f515a2b5c41e8c3c281f794
Sun Junwen <sunjw8888@gmail.com>
2024-04-02 17:13:21 +0800

    SimilarImagesSearch: Only print INFO level logs.

--- SimilarImagesSearch/log_util.py
+++ SimilarImagesSearch/log_util.py
@@ -7,7 +7,7 @@ logger = logging.getLogger('RunTime')
 logging.basicConfig(
     format = '%(asctime)s.%(msecs)03d %(levelname)s [%(filename)s line:%(lineno)d] - %(message)s',
     datefmt = '%Y-%m-%d %H:%M:%S',
-    level = logging.DEBUG,
+    level = logging.INFO,
     handlers = [
         #RotatingFileHandler(logPath, maxBytes=10000000, backupCount=10),
         logging.StreamHandler()
