84bfa7deb43d35c2b9979c468434398873547f31
Sun Junwen <sunjw8888@gmail.com>
2024-06-05 15:19:37 +0800

    RDPPassEncWUI3: Single instance app.

---
+++ RDPPassEncWUI3/RDPPassEncWUI3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.UI.Dispatching;
+using Microsoft.Windows.AppLifecycle;
+
+namespace RDPPassEncWUI3
+{
+    class Program
+    {
+        [STAThread]
+        static async Task Main(string[] args)
+        {
+            WinRT.ComWrappersSupport.InitializeComWrappers();
+            bool isRedirect = await DecideRedirection();
+            if (!isRedirect)
+            {
+                Microsoft.UI.Xaml.Application.Start((p) =>
+                {
+                    var context = new DispatcherQueueSynchronizationContext(
+                        DispatcherQueue.GetForCurrentThread());
+                    SynchronizationContext.SetSynchronizationContext(context);
+                    new App();
+                });
+            }
+
+            return;
+        }
+
+        private static async Task<bool> DecideRedirection()
+        {
+            bool isRedirect = false;
+            AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
+            ExtendedActivationKind kind = args.Kind;
+            AppInstance keyInstance = AppInstance.FindOrRegisterForKey("RDPPassEncWUI3");
+
+            if (keyInstance.IsCurrent)
+            {
+                keyInstance.Activated += OnActivated;
+            }
+            else
+            {
+                isRedirect = true;
+                await keyInstance.RedirectActivationToAsync(args);
+            }
+            return isRedirect;
+        }
+
+        private static void OnActivated(object sender, AppActivationArguments args)
+        {
+            ExtendedActivationKind kind = args.Kind;
+        }
+    }
+}
