50e77a70ce3843fa9d95ae5b1d12cc8cc71ae56d
Sun Junwen <sunjw8888@gmail.com>
2024-07-04 12:14:15 +0800

    ShowWindowsVersion: Implement ShowWindowsVersion.

--- ShowWindowsVersion/ShowWindowsVersion.cpp
+++ ShowWindowsVersion/ShowWindowsVersion.cpp
@@ -1,5 +1,16 @@
-﻿
-int main()
+﻿#include "stdafx.h"
+
+#include "strhelper.h"
+#include "WindowsComm.h"
+
+using namespace std;
+using namespace sunjwbase;
+using namespace WindowsComm;
+
+int wmain(int argc, wchar_t* argv[])
 {
+    tstring tstrWindowsInfo = GetWindowsInfo();
+    wprintf(L"%s\r\n", tstrWindowsInfo.c_str());
+
     return 0;
 }
