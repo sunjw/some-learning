adfca3309191d1ff0fcc0af5423198d0a0a147f3
Sun Junwen <sunjw8888@gmail.com>
2025-01-23 16:07:06 +0800

    BingWallpaper: Some tweak.

--- BingWallpaper/download_bing_wallpaper_all.sh
+++ BingWallpaper/download_bing_wallpaper_all.sh
@@ -2,13 +2,14 @@
 
 ## declare an array variable
 declare -a mkt_arr=("en-us" "zh-cn" "ja-jp" "fr-fr" "it-it" "en-gb" "es-es" "de-de")
+no_promote_string="--no-promote"
 
 cur_dir="$PWD"
 cd "$(dirname "$0")"
 
 no_promote_script_arg="$1"
 all_no_promote=false
-if [ "$no_promote_script_arg" == "--no-promote" ] ; then
+if [ "$no_promote_script_arg" == "$no_promote_string" ] ; then
     all_no_promote=true
 fi
 # echo "$all_no_promote"
@@ -17,13 +18,13 @@ first_mkt=true
 ## now loop through the above array
 for mkt_itr in "${mkt_arr[@]}"
 do
-    arg_no_promote="--no-promote"
+    arg_no_promote="$no_promote_string"
     if [ "$first_mkt" = true ] ; then
         arg_no_promote=""
         first_mkt=false
     fi
     if [ "$all_no_promote" = true ] ; then
-        arg_no_promote="--no-promote"
+        arg_no_promote="$no_promote_string"
     fi
     arg_mkt=" --mkt $mkt_itr"
 
