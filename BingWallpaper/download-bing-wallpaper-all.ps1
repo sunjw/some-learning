45128fc263f4bcc2370fb6bf301c7d69bd7aee0e
Sun Junwen <sunjw8888@gmail.com>
2025-01-23 16:04:12 +0800

    BingWallpaper: Support "--no-promote" in download script.

--- BingWallpaper/download-bing-wallpaper-all.ps1
+++ BingWallpaper/download-bing-wallpaper-all.ps1
@@ -1,11 +1,25 @@
 $MktArr = @("en-us", "zh-cn", "ja-jp", "fr-fr", "it-it", "en-gb", "es-es", "de-de")
+$NoPromoteString = "--no-promote"
+
+$CurDir = Get-Location
+Set-Location (Split-Path -Parent $MyInvocation.MyCommand.Path)
+
+$NoPromoteScriptArg = $args[0]
+$AllNoPromote = $false
+if ($NoPromoteScriptArg -eq $NoPromoteString) {
+    $AllNoPromote = $true
+}
+# Write-Output $AllNoPromote
 
 $FirstMkt = $true
 foreach ($MktItr in $MktArr) {
-    $ArgNoPromote = "--no-promote"
+    $ArgNoPromote = $NoPromoteString
     if ($FirstMkt) {
         $ArgNoPromote = ""
         $FirstMkt = $false
+    }
+    if ($AllNoPromote -eq $true) {
+        $ArgNoPromote = $NoPromoteString
     }
     $ArgMkt=" --mkt $MktItr"
 
@@ -15,3 +29,5 @@ foreach ($MktItr in $MktArr) {
     # python3 bing_wallpaper.py $DownloadArgs
     Start-Process "python3" -ArgumentList "bing_wallpaper.py $DownloadArgs" -NoNewWindow -PassThru -Wait
 }
+
+Set-Location $CurDir
